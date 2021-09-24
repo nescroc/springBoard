898cc3cd49a1b97ce6e67fea1f714ef7d9edfef7
nes <nes0510@gmail.com>
2021-09-24 18:26:05 +0900

    mycommit

---
+++ myboard/src/main/java/sen/spring/polymorphism/BeanFactory.java
@@ -0,0 +1,11 @@
+package sen.spring.polymorphism;
+
+public class BeanFactory {
+	public Object getBean(String beanName) {
+			if(beanName.equals("samsung")) 
+				return new SamsungTV();
+			else if(beanName.equals("lg"))
+				return new LgTV();
+		return null;
+	}
+}
