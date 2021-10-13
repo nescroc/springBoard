59d424fd06c7447fd9649cf4d8182f43dbc66d50
NIS <nes0510@gmail.com>
2021-10-13 20:29:06 +0900

    스프링부트 사용자정의 starter

--- myboard/src/main/java/sen/spring/web/board/BoardVO.java
+++ myboard/src/main/java/sen/spring/web/board/BoardVO.java
@@ -14,8 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.Data;
+
 //@Entity
 //@Table(name="MYBOARD")
+@Data
 public class BoardVO {
 //	@Id
 //	@GeneratedValue
@@ -35,60 +38,5 @@ public class BoardVO {
 //	@Transient
 	@JsonIgnore
 	private MultipartFile uploadFile;
-	
-	public int getSeq() {
-		return seq;
-	}
-	public void setSeq(int seq) {
-		this.seq = seq;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Date getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-	public int getCnt() {
-		return cnt;
-	}
-	public void setCnt(int cnt) {
-		this.cnt = cnt;
-	}
-	public String getSearchCondition() {
-		return searchCondition;
-	}
-	public void setSearchCondition(String searchCondition) {
-		this.searchCondition = searchCondition;
-	}
-	public String getSearchKeyword() {
-		return searchKeyword;
-	}
-	public void setSearchKeyword(String searchKeyword) {
-		this.searchKeyword = searchKeyword;
-	}
-	public MultipartFile getUploadFile() {
-		return uploadFile;
-	}
-	public void setUploadFile(MultipartFile uploadFile) {
-		this.uploadFile = uploadFile;
-	}
 	
 }
